18406bdfb3716bc0d022f1910e5c5696da20e30b
JKukielski <jankukielski1@gmail.com>
2024-01-16 19:50:49 +0100

    implemented react-router-dom with basic routes for home, login and register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <HomePage />
-      {/* <Login />
-      <Register /> */}
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
