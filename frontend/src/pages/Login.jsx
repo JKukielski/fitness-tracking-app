f2e78e80da9306ff56fd9ab1da74c0a6917f6777
JKukielski <jankukielski1@gmail.com>
2024-01-16 19:54:52 +0100

    added basic structure for input field useState

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {};
+
   return (
     <div>
-      <label htmlFor="email">
-        Email
-        <input type="email" id="email" />
-      </label>
-      <label htmlFor="password">
-        Password
-        <input type="password" id="password" />
-      </label>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+      </form>
     </div>
   );
 };
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,18 +1,46 @@
+import { useState } from 'react';
+
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {};
+
   return (
     <div>
-      <label htmlFor="email">
-        Email
-        <input type="email" id="email" />
-      </label>
-      <label htmlFor="username">
-        Username
-        <input type="text" id="username" />
-      </label>
-      <label htmlFor="password">
-        Password
-        <input type="password" id="password" />
-      </label>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="username">
+          Username
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+      </form>
     </div>
   );
 };
