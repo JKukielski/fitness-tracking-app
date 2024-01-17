26dd7ea60b13b335558c2d136301dc141aa8c72e
JKukielski <jankukielski1@gmail.com>
2024-01-17 19:24:16 +0100

    functioning API call (need to fix password validation)

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,63 @@
 import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({
+    email: '',
+    username: '',
+    password: '',
+  });
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {};
+  const isPasswordValid = (password) => {
+    return (
+      password.length >= 8 && /[a-zA-Z]/.test(password) && /\d/.test(password)
+    );
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'http://localhost:3001/api/auth/register',
+        {
+          email,
+          username,
+          password,
+        }
+      );
+
+      if (!isPasswordValid(password)) {
+        setErrors({
+          ...errors,
+          password:
+            'Password must be at least 8 characters long and include letters and numbers',
+        });
+      }
+      if (response) {
+        navigate('/login');
+      }
+    } catch (err) {
+      if (err.response) {
+        console.error('Registration failed with status:', err.response.status);
+
+        setErrors({
+          email: err.response.data.errors?.email || '',
+          username: err.response.data.errors?.username || '',
+          password: '',
+        });
+      } else if (err.request) {
+        console.error('No response received');
+      } else {
+        console.error('Error setting up the request:', err.message);
+      }
+    }
+  };
 
   return (
     <div>
@@ -20,6 +72,7 @@ const Register = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
+        <p>{errors.email}</p>
         <label htmlFor="username">
           Username
           <input
@@ -30,6 +83,8 @@ const Register = () => {
             onChange={(e) => setUsername(e.target.value)}
           />
         </label>
+        <p>{errors.username}</p>
+
         <label htmlFor="password">
           Password
           <input
@@ -40,6 +95,9 @@ const Register = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
+        <p>{errors.password}</p>
+
+        <button type="submit">Register</button>
       </form>
     </div>
   );
