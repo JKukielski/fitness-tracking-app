957771c1d8246b685d50cc754105ce7d0d28d3b6
JKukielski <jankukielski1@gmail.com>
2024-01-17 18:34:20 +0100

    implemented endpoint for login and redirect to home page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,43 @@
+import axios from 'axios';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../state/userSlice';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'http://localhost:3001/api/auth/login',
+        {
+          email,
+          password,
+        }
+      );
+
+      if (response) {
+        dispatch(
+          login({
+            user: response.data,
+            token: response.data.token,
+          })
+        );
+
+        navigate('/');
+      }
+
+      // console.log('Login successful', response.data.user.email);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
@@ -29,6 +62,7 @@ const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
