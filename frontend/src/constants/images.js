c7fcadb9ae3bd502ff4394c532c19eda67eac639
JKukielski <jankukielski1@gmail.com>
2024-01-22 20:38:45 +0100

    uploaded images for user page

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,5 +1,9 @@
 import loginRegister from '../assets/LoginRegister.svg';
+import male_user from '../assets/male_user.jpg';
+import female_user from '../assets/female_user.jpg';
 
 export default {
   loginRegister,
+  male_user,
+  female_user,
 };
