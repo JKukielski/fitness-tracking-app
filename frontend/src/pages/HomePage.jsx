a6c08f248b49db349d2ac8d066df8f2bd0f2d243
JKukielski <jankukielski1@gmail.com>
2024-01-26 22:11:41 +0100

    Setup for home page detail carousel

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import '../styles/HomePage.css';
 import Navbar from '../components/Navbar';
+import CarouselComponent from '../components/CarouselComponent';
 
 const HomePage = () => {
   return (
     <div className="home-container">
       <Navbar />
+      <CarouselComponent />
     </div>
   );
 };
