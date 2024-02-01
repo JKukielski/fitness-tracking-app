3eb071822cc6326a4f5db45ef9dc9abf50a1cbd1
JKukielski <jankukielski1@gmail.com>
2024-02-01 19:09:32 +0100

    Taregt input structure and styling

--- frontend/src/components/TargetInput.jsx
+++ frontend/src/components/TargetInput.jsx
@@ -1,7 +1,25 @@
 import '../styles/TargetInput.css';
 
 const TargetInput = () => {
-  return <div className="target-container"></div>;
+  return (
+    <form className="target-container">
+      <p className="measurement-heading">Enter your target weight</p>
+      <label htmlFor="weight" className="measurement-label">
+        Target weight
+        <input
+          type="number"
+          name="target"
+          id="target"
+          className="measurement-input"
+        />
+      </label>
+      <div className="measurement-button-container">
+        <button type="submit" className="primary-button primary-button-bm">
+          Submit target
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default TargetInput;
