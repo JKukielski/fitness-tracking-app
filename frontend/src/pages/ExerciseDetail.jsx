c655e48ff355c8cc31abf463355c0c92fca1654d
JKukielski <jankukielski1@gmail.com>
2024-02-14 17:05:16 +0100

    Initial styling

--- frontend/src/pages/ExerciseDetail.jsx
+++ frontend/src/pages/ExerciseDetail.jsx
@@ -4,6 +4,8 @@ import { exerciseOptions, fetchData } from '../utils/fetchExerciseData';
 import Navbar from '../components/Navbar';
 import { CgGym } from 'react-icons/cg';
 import { BiBody, BiTargetLock } from 'react-icons/bi';
+import '../styles/ExerciseDetail.css';
+
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [similarMuscle, setSimilarMuscle] = useState([]);
@@ -20,31 +22,43 @@ const ExerciseDetail = () => {
     };
 
     fetchExerciseDetail();
-    console.log(exerciseDetail);
   }, [id]);
 
   return (
-    <div>
+    <>
       <Navbar />
-      <img src={exerciseDetail.gifUrl} alt="" />
-      <p className="detail-desc">
-        The <span>{exerciseDetail.name}</span> is one of the best exercises to
-        target your {exerciseDetail.target}. It will help you improve your mood
-        and gain energy.
-      </p>
-      <div className="detail-element">
-        <BiBody />
-        <p className="detail-element-text">{exerciseDetail.bodyPart}</p>
+      <div className="detail-container">
+        <div className="detail-inner-container">
+          <img src={exerciseDetail.gifUrl} alt="" className="detail-image" />
+          <div className="detail-content">
+            <p className="detail-desc">
+              The <span>{exerciseDetail.name}</span> is one of the best
+              exercises to target your {exerciseDetail.target}. It will help you
+              improve your mood and gain energy.
+            </p>
+            <div className="detail-element">
+              <div className="detail-icon-container">
+                <BiBody className="detail-icon" />
+              </div>
+              <p className="detail-element-text">{exerciseDetail.bodyPart}</p>
+            </div>
+            <div className="detail-element">
+              <div className="detail-icon-container">
+                <BiTargetLock className="detail-icon" />
+              </div>
+              <p className="detail-element-text">{exerciseDetail.target}</p>
+            </div>
+            <div className="detail-element">
+              <div className="detail-icon-container">
+                <CgGym className="detail-icon" />
+              </div>
+              <p className="detail-element-text">{exerciseDetail.equipment}</p>
+            </div>
+          </div>
+        </div>
+        <div className="secondary-container"></div>
       </div>
-      <div className="detail-element">
-        <BiTargetLock />
-        <p className="detail-element-text">{exerciseDetail.target}</p>
-      </div>
-      <div className="detail-element">
-        <CgGym />
-        <p className="detail-element-text">{exerciseDetail.equipment}</p>
-      </div>
-    </div>
+    </>
   );
 };
 
